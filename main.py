c0f9700e0046b7f25be66e79430c57e181620188
jakeleslie <43657120+jakeleslie@users.noreply.github.com>
2021-11-30 15:56:07 -0500

    Get's information, and specific typing

--- main.py
+++ main.py
@@ -1,28 +1,41 @@
 from fastapi import FastAPI, status, Response
+import simplejson as json
 
 app = FastAPI()
 
-users = {
-    "Serdar": {
-        "id" : 32,
-        "full_name" : "Serdar Yegulalp"
-    },
-    "Jake": {
-        "id": 21,
-        "full_name" : "Jake Leslie"
-    }
-}
+with open('pokedex.json') as fin:
+    content = fin.read()
+    obj = json.loads(content)
+
 
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
 
-
-@app.get("/user/{username}")
-async def get_user(username, response: Response):
+@app.get("/id/{id}")
+async def get_id(id, response: Response):
     try:
-        result = users[username]
+        res = [x for x in obj if x['id'] == int(id)]
+
+        if len(res) == 0:
+            return 401
+        else:
+            
+            return res[0]
+
     except KeyError:
-        result = {"err":f"No such user: {username}"}
+        result = {"err":f"No such {id}"}
         response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
     return result
+# returns the type of a specific id
+@app.get("/id/{id}/type")
+async def get_type(id, response: Response):
+    try:
+        res = [x['type'] for x in obj if x['id'] == int(id)]
+
+        return res[0]
+    except KeyError:
+        result = {"err":f"No such {id} "}
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+    return result
+
